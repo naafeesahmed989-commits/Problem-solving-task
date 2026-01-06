d8ee163f34714afa3ca20656a79ccf0934eb6b80
Nafisa <naafeesahmed989@gmail.com>
2026-01-06 02:05:58 +0200

    Update task in lists.py

--- task in lists.py
+++ task in lists.py
@@ -1,13 +1,14 @@
-# we should remove the duplicate in the list
+# the task is to remove the duplicate in the list
 numbers=[1,1,3,4,5,5,6]
-# i will create a new list so i can store the item in it
+# i will create a new list so i can store the items in it
 numbers2=[]
 # using for loop to move through items
 for item in numbers:
     # a condition to check if we already have this num or not
     if item not in numbers2:
-        # if not then add it to the list
+        # if it is not there then add it to the list
         numbers2.append(item)
 print(numbers2)
+
 
 
