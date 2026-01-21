4894d346523bf793669cb2c9acc95dd7d60af873
Nafisa <naafeesahmed989@gmail.com>
2026-01-21 20:14:16 +0200

    Update Count Elements with strictly smaller and greater elements.py

--- leetcode/Count Elements with strictly smaller and greater elements.py
+++ leetcode/Count Elements with strictly smaller and greater elements.py
@@ -1,1 +1,14 @@
-
+class Solution(object):
+    def countElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        smallest=min(nums)
+        greatest=max(nums)
+        count=0
+        for num in nums:
+            if num > smallest and num < greatest :
+                count+=1
+        
+        return count
